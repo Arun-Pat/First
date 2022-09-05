4eb08d4670e4256fb8d4077100daa3b0662a7048
ArunP <arunp_mail@yahoo.com>
2022-09-05 20:31:48 +0530

    commiting sum.java

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 //import java.util.*;
 public class sum {
     public static void main(String [] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World. This is first Program");
         
     }
 }
